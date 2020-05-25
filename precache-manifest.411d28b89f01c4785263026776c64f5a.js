890c8cfa4f8fdd5163fedfd8ed21468a4b56564d
diveshjumani <jumanidivesh@gmail.com>
2020-05-25 17:01:02 -0400

    Updates

---
+++ precache-manifest.411d28b89f01c4785263026776c64f5a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18aec8a908ee8fbde5304c03eb52ef54",
+    "url": "/gtfintech/index.html"
+  },
+  {
+    "revision": "65302966228f91668ec7",
+    "url": "/gtfintech/static/css/2.d805e148.chunk.css"
+  },
+  {
+    "revision": "18dc79fd8400bedc2bce",
+    "url": "/gtfintech/static/css/main.358d6abd.chunk.css"
+  },
+  {
+    "revision": "65302966228f91668ec7",
+    "url": "/gtfintech/static/js/2.a0dc86c8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/gtfintech/static/js/2.a0dc86c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18dc79fd8400bedc2bce",
+    "url": "/gtfintech/static/js/main.89d59882.chunk.js"
+  },
+  {
+    "revision": "97a79a0606e5611a86f3",
+    "url": "/gtfintech/static/js/runtime-main.acddfa5a.js"
+  },
+  {
+    "revision": "750ec09c2488ef91218c6baa844502fd",
+    "url": "/gtfintech/static/media/GT_White.750ec09c.png"
+  },
+  {
+    "revision": "ade237cf6e0e7f67f4ec14f295a1eb18",
+    "url": "/gtfintech/static/media/GeorgiaTechLogo-RGB-Gray.ade237cf.png"
+  }
+]);
