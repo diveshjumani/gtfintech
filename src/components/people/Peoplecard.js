8ae9af9c9abb2da078fafc9b96aabd0fc96966f1
diveshjumani <jumanidivesh@gmail.com>
2020-06-26 15:32:47 -0400

    ..

--- src/components/people/Peoplecard.js
+++ src/components/people/Peoplecard.js
@@ -5,12 +5,12 @@ const Peoplecard = ({website, imagef, name, role}) => {
     return(
         <div className = ' dib br3 pa3 ma2 grow bw2'>
                 <a target="_blank" rel="noopener noreferrer" href = {website}>
-                    <img style={{ width: '185px', height: '220px'}} alt="professor" src = {process.env.PUBLIC_URL + imagef} />
+                    <img class = "cardimgstyling" alt="professor" src = {process.env.PUBLIC_URL + imagef} />
                 </a>
                 
                 <div style={{ width: '190px'}}>
-                    <h5><b>{name}</b></h5>   
-                    <h5>{role}</h5>
+                    <h5>{name}</h5>   
+                    <h6>{role}</h6>
                 </div>     
         </div>
     )
