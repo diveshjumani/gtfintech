6eb02e10aca9592ecac3b2f2a00e09ddc5f8302d
diveshjumani <jumanidivesh@gmail.com>
2020-06-27 21:00:21 -0400

    Research Update

--- src/components/people/Alumni.js
+++ src/components/people/Alumni.js
@@ -3,15 +3,15 @@ export const Alumni = [
         id: 1,
         name: 'Steven Chong Xiao',
         role: 'Assistant Professor',
-        website: '',
+        website: 'https://sites.google.com/site/stevencxiao',
         imagef: '/images/steven chong xiao profile photo.jpg'
     },
     {
         id: 2,
-        name: 'Student Name',
-        role: 'Specialisation',
-        website: '',
-        imagef: '/images/career_buzz_logo.png'
+        name: 'Linghang Zeng',
+        role: 'Assistant Professor',
+        website: 'https://sites.google.com/view/linghang-zeng',
+        imagef: '/images/Linghang Zeng.jpg'
     },
     {
         id: 3,
